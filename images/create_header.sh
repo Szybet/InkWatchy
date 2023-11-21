9c39656399ca7e8d38079471a8c2dc83a06c1731
Szybet <53944559+Szybet@users.noreply.github.com>
2023-11-21 16:06:32 +0100

    cleaning + img generation

---
+++ images/create_header.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm images.h 1>/dev/null 2>/dev/null
+touch images.h
+
+echo -e "#ifndef IMAGES_H" >> images.h
+echo -e "#define IMAGES_H" >> images.h
+echo -e '' >> images.h
+
+for f in *
+do
+    if [[ $f == *".sh"* ]] || [[ $f == *".h"* ]]; then
+        continue
+    fi
+
+    fne="${f%.*}Img"
+
+    echo "Processing $f" # always double quote "$f" filename
+
+    convert $f h:- | sed 's/MagickImage/'$fne'/g' | tail -n +4 >> images.h
+    echo -e '' >> images.h
+
+done
+
+echo "#endif" >> images.h
+
+mv images.h ../src/defines/
