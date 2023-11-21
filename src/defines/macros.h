4412b9960b211b7f78af72d36f57b107672637a2
Szybet <53944559+Szybet@users.noreply.github.com>
2023-11-21 18:59:55 +0100

    Debugging template

---
+++ src/defines/macros.h
@@ -0,0 +1,12 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+// https://stackoverflow.com/questions/68242576/macro-which-will-not-compile-the-function-if-not-defined
+// This way, we don't waste memory!
+#if DEBUG
+#define isDebug(command) command;
+#else
+#define isDebug(...) NULL
+#endif
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
+#include <Arduino.h>
 
+#include "defines/defines.h"
+#include "defines/macros.h"
 
 void setup() {
-  
+  isDebug(Serial.begin(230400));
 }
 
 void loop() {
