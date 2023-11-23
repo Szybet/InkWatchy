6edd51648680d13343e659e107346a6a3a359f8e
kitkat <78702592+KitKat-ok@users.noreply.github.com>
2023-11-23 21:34:58 +0100

    Init hardware

--- src/defines/defines.h
+++ src/defines/defines.h
@@ -1,8 +1,12 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
+#include <Arduino.h>
+
 #include "config.h"
 #include "images.h"
 #include "macros.h"
+
+#include "../hardware/hardware.h"
 
 #endif
---
+++ src/hardware/hardware.h
@@ -0,0 +1,10 @@
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+void setupHardware();
+
+#if DEBUG
+void dumpButtons();
+#endif
+
+#endif
