dbe94e8ee28cc84a2ecf7a68427914372d2927ec
kitkat <78702592+KitKat-ok@users.noreply.github.com>
2023-11-24 22:18:02 +0100

    epd border

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include "defines/defines.h"
 #include "defines/macros.h"
 
-void setup() {
+void setup()
+{
   setupHardware();
-
 }
 
-void loop() {
-  isDebug(dumpButtons());
+void loop()
+{
+#if DEBUG
+  dumpButtons();
+  showSetupResults();
+#endif
+  display.display(FULL_UPDATE);
+  display.setFullWindow();
+  delay(10000);
 }
-
