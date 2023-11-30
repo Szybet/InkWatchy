a303fcfe6c039236344e4fbec3ec5afd277d6b54
kitkat <78702592+KitKat-ok@users.noreply.github.com>
2023-11-30 19:12:46 +0100

    wifi debug loop

--- src/ui/functionsUi.cpp
+++ src/ui/functionsUi.cpp
@@ -5,50 +5,74 @@ const GFXfont *font;
 int maxHeight;
 int textSize;
 
-void setTextSize(int i) {
-    textSize = i;
-    display.setTextSize(i);
+void setTextSize(int i)
+{
+  textSize = i;
+  display.setTextSize(i);
 }
 
-void setFont(const GFXfont *fontTmp) {
-    font = fontTmp;
-    display.setFont(font);
+void setFont(const GFXfont *fontTmp)
+{
+  font = fontTmp;
+  display.setFont(font);
 }
 
-void writeLine(String strToWrite, int cursorX, uint16_t* currentHeight) {
-    display.setCursor(cursorX, *currentHeight);
-    display.print(strToWrite);
-    *currentHeight = *currentHeight + maxHeight;
+void writeLine(String strToWrite, int cursorX, uint16_t *currentHeight)
+{
+  display.setCursor(cursorX, *currentHeight);
+  display.print(strToWrite);
+  *currentHeight = *currentHeight + maxHeight;
 }
 
-void writeTextReplaceBack(String str, int16_t x, int16_t y, uint16_t frColor, uint16_t bgColor, int tolerance) {
-    log("Drawing bitmap with text: " + str + " at: " + String(x) + "x" + String(y));
+void writeTextReplaceBack(String str, int16_t x, int16_t y, uint16_t frColor, uint16_t bgColor, int tolerance)
+{
+  log("Drawing bitmap with text: " + str + " at: " + String(x) + "x" + String(y));
 
-    uint16_t w, h;
-    int16_t xt, yt; // tmp, don't need it
-    display.getTextBounds(str, 1, 100, &xt, &yt, &w, &h);
-    log("w: " + String(w));
-    log("h: " + String(h));
-    w = w + tolerance;
-    h = h;
-    GFXcanvas1 canvasTmp(w, h);
-    canvasTmp.fillScreen(false); // Clear canvas
-    canvasTmp.setFont(font);
-    canvasTmp.setTextSize(textSize);
-    canvasTmp.setCursor(0, h - 1);
-    canvasTmp.print(str);
-    display.drawBitmap(x, y - h + (tolerance / 3), canvasTmp.getBuffer(), w, h, frColor, bgColor);
+  uint16_t w, h;
+  int16_t xt, yt; // tmp, don't need it
+  display.getTextBounds(str, 1, 100, &xt, &yt, &w, &h);
+  log("w: " + String(w));
+  log("h: " + String(h));
+  w = w + tolerance;
+  h = h;
+  GFXcanvas1 canvasTmp(w, h);
+  canvasTmp.fillScreen(false); // Clear canvas
+  canvasTmp.setFont(font);
+  canvasTmp.setTextSize(textSize);
+  canvasTmp.setCursor(0, h - 1);
+  canvasTmp.print(str);
+  display.drawBitmap(x, y - h + (tolerance / 3), canvasTmp.getBuffer(), w, h, frColor, bgColor);
 }
 
-void centerText(String text, uint16_t* currentHeight, int x) {
+void centerText(String text, uint16_t *currentHeight, int x)
+{
   int16_t x1, y1;
   uint16_t w, h;
   display.getTextBounds(text.c_str(), 0, 0, &x1, &y1, &w, &h);
-  if (x == -1) {
+  if (x == -1)
+  {
     x = (display.width() - w) / 2;
   }
   display.setCursor(x, *currentHeight);
   display.print(text);
   *currentHeight = *currentHeight + maxHeight;
 }
+
+void writeTextCenterReplaceBack(String str, uint16_t y, uint16_t frColor, uint16_t bgColor, int tolerance)
+{
+  uint16_t w, h;
+  int16_t xt, yt; // tmp, don't need it
+  display.getTextBounds(str.c_str(), 0, 0, &xt, &yt, &w, &h);
+  w += tolerance;
+  GFXcanvas1 canvasTmp(w, h);
+  canvasTmp.fillScreen(false); // Clear canvas
+  canvasTmp.setFont(font);
+  canvasTmp.setTextSize(textSize);
+  int x = (display.width() - w) / 2;
+  canvasTmp.setCursor(x, h - 1);
+  canvasTmp.print(str);
+
+  display.drawBitmap(x, y - h + (tolerance / 3), canvasTmp.getBuffer(), w, h, frColor, bgColor);
+}
+
 
--- src/ui/wifiDebug.cpp
+++ src/ui/wifiDebug.cpp
@@ -3,13 +3,13 @@
 #if DEBUG == 1 || DEBUG_MENUS == 1
 int cursorXwifi = 0;
 int StatusHeight;
-#define wifiTextSize 1
+#define TextSize 1
 
 void initWifiDebugDisplay() {
     int16_t x, y;
     uint16_t w, h;
     setFont(&FreeSansBold9pt7b);
-    setTextSize(wifiTextSize);
+    setTextSize(TextSize);
     display.setCursor(cursorXwifi, 1);
     String menuName = "Debug Menu: Wifi";
     display.getTextBounds(menuName, cursorXwifi, 1, &x, &y, &w, &h);
@@ -21,11 +21,22 @@ void initWifiDebugDisplay() {
 
     display.fillRect(0, currentHeight, display.width(), 3, GxEPD_BLACK);
     currentHeight = currentHeight + maxHeight;
-
     centerText("Wifi status: ", &currentHeight);
-    StatusHeight = currentHeight - maxHeight;
     centerText(String(wifiStatus()), &currentHeight);
     StatusHeight = currentHeight - maxHeight;
+    display.fillRect(0, currentHeight - h/2 - 2, display.width(), 1, GxEPD_BLACK);
+    currentHeight = currentHeight + 5;
+    writeLine("Turn wifi on",cursorXwifi, &currentHeight);
+    writeLine("Turn wifi off",cursorXwifi, &currentHeight);
     display.display(FULL_UPDATE);
+}
+
+void loopWifiDebugDisplay()
+{
+    setFont(&FreeSansBold9pt7b);
+    setTextSize(TextSize);
+   writeTextCenterReplaceBack(String(wifiStatus()), StatusHeight);
+    display.display(PARTIAL_UPDATE);
+
 }
 #endif
