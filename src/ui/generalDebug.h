0d7fdd65b26202fcdd6e6cf784b4aea5fe6273ff
kitkat <78702592+KitKat-ok@users.noreply.github.com>
2023-12-23 19:34:22 +0100

    General debug

---
+++ src/ui/generalDebug.h
@@ -0,0 +1,12 @@
+#ifndef GENERALDEBUG_H
+#define GENERALDEBUG_H
+
+#include "defines/defines.h"
+
+void initGeneralDebugDisplay();
+void loopGeneralDebugDisplay();
+void GeneralDebugLoop();
+void GeneralDebug();
+
+
+#endif
