02fbcf06f04fc5ccac295d1213bcf0fa1fe42b40
Szybet <53944559+Szybet@users.noreply.github.com>
2024-01-02 13:43:59 +0100

    update src/hardware/display.h

--- src/hardware/display.h
+++ src/hardware/display.h
@@ -7,7 +7,7 @@ extern RTC_DATA_ATTR GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display;
 
 void initDisplay(bool isFromWakeUp);
 extern bool dUChange; // Display update change boolean, to simplify the code
-void disUp(bool reallyUpdate = false);
+void disUp(bool reallyUpdate = false, bool ignoreCounter = false);
 
 #if DEBUG
 
