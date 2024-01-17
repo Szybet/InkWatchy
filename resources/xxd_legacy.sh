2c085c3a1db3a38fb6a814c44cd1b4ea0934e33a
Kuratius <Kuratius@gmx.net>
2024-01-17 18:26:37 +0100

    remove line that references image generation and change type of size variable to one that is guaranteed to work for arbitrary array sizes

--- resources/xxd_legacy.sh
+++ resources/xxd_legacy.sh
@@ -11,9 +11,8 @@ xxd_wrapper(){
     echo -e -n "const unsigned char  ${1} [] = {\n" 
 
     echo "$input_data"
-    echo -e "};\nconst unsigned int ${1}d =(sizeof($1)/sizeof(${1}[0])) ;\n"
+    echo -e "};\nconst size_t ${1}d =(sizeof(${1})/sizeof(${1}[0])) ;\n"
 
-    echo -n -e "const ImageDef ${1}Pack = {${1}, ${2}_WIDTH, ${2}_HEIGHT};\n" 
 
 
 }
