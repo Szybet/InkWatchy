0e8b9da0e2853cb271f7b373d33c9b3c4d223d5a
Szybet <53944559+Szybet@users.noreply.github.com>
2024-05-14 23:52:36 +0200

    I need to adjust block size inside the code too

--- resources/fs/flashFs.sh
+++ resources/fs/flashFs.sh
@@ -3,5 +3,7 @@
 size=$(<out/size.txt tr -d '\n')
 offset=$(<out/offset.txt tr -d '\n')
 
-./mklittlefs -c littlefs -s $size out/fs.bin
+# https://github.com/earlephilhower/mklittlefs/blob/6e2fa1789757d52455e08f9deb33b8992ba96e07/main.cpp#L817-L818
+# Block size adjusted for book
+./mklittlefs -b 256 -c littlefs -s $size out/fs.bin
 esptool.py write_flash $offset out/fs.bin
