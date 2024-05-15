eaf209d9b1127e88ff32cbc41b958f494a4bb0b0
Szybet <53944559+Szybet@users.noreply.github.com>
2024-05-15 18:01:46 +0200

    book progress

---
+++ resources/books/convertBooks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf out/
+mkdir -p out/
+
+for file in *; do
+    if [ ! -d "$file" ] && [[ "$file" =~ \.(epub|pdf)$ ]]; then
+        echo "Processing file: $file"
+        file_no_ext="${file%.*}"
+        mutool convert -F text -o "out/$file_no_ext" "$file"
+        sed -i ':a;N;$!ba;s/\n/ /g;s/ \+/ /g' "out/$file_no_ext"
+    fi
+done
