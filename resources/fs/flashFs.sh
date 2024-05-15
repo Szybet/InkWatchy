1bc5fc5ec071e38c2ac1bb9c7dcf546d3886ae33
Szybet <53944559+Szybet@users.noreply.github.com>
2024-05-15 18:01:46 +0200

    book progress

--- resources/fs/flashFs.sh
+++ resources/fs/flashFs.sh
@@ -5,5 +5,7 @@ offset=$(<out/offset.txt tr -d '\n')
 
 # https://github.com/earlephilhower/mklittlefs/blob/6e2fa1789757d52455e08f9deb33b8992ba96e07/main.cpp#L817-L818
 # Block size adjusted for book
-./mklittlefs -b 256 -c littlefs -s $size out/fs.bin
+# https://electronics.stackexchange.com/questions/588293/esp32-littlefs-change-block-size
+# Fuck
+./mklittlefs -c littlefs -s $size out/fs.bin
 esptool.py write_flash $offset out/fs.bin
