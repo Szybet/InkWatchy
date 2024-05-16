f9137d057b3de8b6810816e66f2cb0b0532a5e95
Szybet <53944559+Szybet@users.noreply.github.com>
2024-05-16 22:12:31 +0200

    book progress

--- resources/books/convertBooks.sh
+++ resources/books/convertBooks.sh
@@ -11,3 +11,6 @@ for file in *; do
         sed -i ':a;N;$!ba;s/\n/ /g;s/ \+/ /g' "out/$file_no_ext"
     fi
 done
+
+rm -rf ../fs/littlefs/book 1>/dev/null 2>/dev/null
+cp -r out/ ../fs/littlefs/book
---
+++ src/ui/book/bookSelector.h
@@ -0,0 +1,9 @@
+#ifndef BOOK_SELECTOR_H
+#define BOOK_SELECTOR_H
+
+#include "../../defines/defines.h"
+
+void initBookSelector();
+void selectedBook();
+
+#endif
