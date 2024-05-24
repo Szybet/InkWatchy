1566fbc8fca6bb28c915ef50cb56bd523aa6c0ae
Szybet <53944559+Szybet@users.noreply.github.com>
2024-05-24 22:32:59 +0200

    demo setup script

--- resources/tools/other/demoMaker.sh
+++ resources/tools/other/demoMaker.sh
@@ -3,3 +3,42 @@ cp -r resources/demo/* resources/personal/
 cd resources/
 export PLATFORMIO_ENV_NAME="min"
 ./generate.sh
+
+old_string="#define VAULT_PASSWORD \"\""
+new_string="#define VAULT_PASSWORD \"1\""
+filename="src/defines/confidential.h"
+
+sed -i "s/$old_string/$new_string/g" "$filename"
+
+old_string="#define VAULT 0"
+new_string="#define VAULT 1"
+filename="src/defines/config.h"
+
+sed -i "s/$old_string/$new_string/g" "$filename"
+
+old_string="#define BOOK 0"
+new_string="#define BOOK 1"
+filename="src/defines/config.h"
+
+sed -i "s/$old_string/$new_string/g" "$filename"
+
+pio run -e min
+
+cd resources/tools/fs/
+python3 generatePartTable.py
+cd ../../../
+
+pio run -e min
+pio run -e min -t upload
+
+sleep 5
+
+cd resources/tools/fs/
+./flashFs.sh
+cd ../../../
+
+sleep 5
+
+cd resources/tools/other/
+mkdir out/
+esptool.py read_flash 0x00000 0x400000 out/demo.bin
