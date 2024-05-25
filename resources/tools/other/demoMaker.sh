2610654f8766f402eda48e530099f8333802b43c
Szybet <53944559+Szybet@users.noreply.github.com>
2024-05-25 11:44:47 +0200

    actually this

--- resources/tools/other/demoMaker.sh
+++ resources/tools/other/demoMaker.sh
@@ -44,6 +44,6 @@ sleep 10
 
 cd resources/tools/other/
 mkdir out/
-esptool.py read_flash 0x10000 0x3F0000 out/demo.bin
+esptool.py read_flash 0x00000 0x400000 out/demo.bin
 # esptool.py erase_flash
-# esptool.py write_flash 0x10000 <the_file>
+# esptool.py write_flash 0x00000 <the_file>
