b70f24401a0e4447292b7f1b768b4d3ce5b903d5
Szybet <53944559+Szybet@users.noreply.github.com>
2024-05-27 00:41:56 +0200

    platformio doesn't select the port

--- resources/tools/other/demoMaker.sh
+++ resources/tools/other/demoMaker.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+source resources/tools/globalFunctions.sh
+
+serial_port=$(extract_serial_port)
+echo "Serial port detected: $serial_port"
+
 pio pkg install -e min
 rm -rf resources/personal/books/* resources/personal/vault/*
 cp -r resources/demo/* resources/personal/
@@ -39,8 +44,8 @@ PLATFORMIO_ENV_NAME="min" python3 generatePartTable.py
 cd ../../../
 
 pio run -e min
-resources/tools/other/in/esptool erase_flash
-pio run -e min -t upload
+resources/tools/other/in/esptool -p $serial_port erase_flash
+pio run -e min -t upload --upload-port $serial_port
 
 sleep 10
 
@@ -51,11 +56,11 @@ cd ../../../
 sleep 10
 
 cd resources/tools/other/
-in/esptool read_flash 0x00000 0x400000 out/demo-program.bin
+in/esptool -p $serial_port read_flash 0x00000 0x400000 out/demo-program.bin
 python ../fs/cleanNVS.py ../fs/in/partitions.csv out/demo-program.bin
 sleep 5
-in/esptool erase_flash
+in/esptool -p $serial_port erase_flash
 sleep 5
-in/esptool write_flash 0x00000 out/demo-program.bin
+in/esptool -p $serial_port write_flash 0x00000 out/demo-program.bin
 cd ../../../
 zip -r -9 resources/tools/other/out/demo-source.zip .
