922a17f1e1622e620e16cedeef8cb87797bbc6b9
Szybet <53944559+Szybet@users.noreply.github.com>
2024-06-26 23:18:02 +0200

    add ninja

--- resources/tools/other/setup/packages.sh
+++ resources/tools/other/setup/packages.sh
@@ -23,7 +23,7 @@ imagemagick_from_source
 
 if command -v dpkg &> /dev/null; then
     echo "Detected a debian based system"
-    debian_packages=("mupdf-tools" "xxd" "jq" "python3-unidecode" "libfreetype-dev" "libfreetype6" "libfreetype6-dev" "python3-venv" "imagemagick" "libltdl-dev" "make" "build-essential" "automake")
+    debian_packages=("mupdf-tools" "xxd" "jq" "python3-unidecode" "libfreetype-dev" "libfreetype6" "libfreetype6-dev" "python3-venv" "imagemagick" "libltdl-dev" "make" "build-essential" "automake" "ninja-build")
     missing_packages=()
 
     for pkg in "${debian_packages[@]}"; do
