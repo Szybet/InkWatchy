a4d7afdfbb494c5d2f6ecf1dec437896f56040af
Szybet <53944559+Szybet@users.noreply.github.com>
2024-06-28 19:28:30 +0000

    devcontainer

--- resources/tools/buildTime/preBuild.sh
+++ resources/tools/buildTime/preBuild.sh
@@ -5,6 +5,14 @@ source resources/tools/globalFunctions.sh
 pio_env=$(get_pio_env .vscode/launch.json)
 
 cd .pio/build/$pio_env
-ninja || true
-ninja || true
-ninja || true
+ninja -k 0 || true
+#ninja || true
+#ninja || true
+
+
+FILE="~/.platformio/penv/.espidf-5.1.2/lib/python3.12/site-packages/pyserial-3.5.dist-info"
+
+if [ ! -e "$FILE" ]; then
+    echo "Pyserial doesn't exist, trying to install it..."
+    /root/.platformio/penv/.espidf-5.1.2/bin/python -m pip install pyserial
+fi
