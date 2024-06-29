1fad71db0ea07ea1bc95d29a147362a2bf6ccad8
Szybet <53944559+Szybet@users.noreply.github.com>
2024-06-29 16:32:33 +0000

    no more warnings

--- resources/tools/buildTime/preBuild.py
+++ resources/tools/buildTime/preBuild.py
@@ -1,3 +1,8 @@
 import subprocess
+Import("env")
+
+# Because cc1: warning: command-line option '-Wno-overloaded-virtual' is valid for C++/ObjC++ but not for C
+env.Append(CXXFLAGS=["-Wno-overloaded-virtual"])
+
 script_path = 'resources/tools/buildTime/preBuild.sh'
 subprocess.run(['bash', script_path])
