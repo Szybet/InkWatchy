993d3e39b2b821644516c30ccd82d025ee4d091f
Szybet <53944559+Szybet@users.noreply.github.com>
2024-07-07 13:04:44 +0000

    fixed env detection once and for all

--- resources/tools/buildTime/preBuild.py
+++ resources/tools/buildTime/preBuild.py
@@ -1,5 +1,18 @@
 import subprocess
+import os
 Import("env")
+
+#print(env.Dump())
+
+pio_env = env.get('PIOENV', '')
+print("Current pio env: " + str(pio_env))
+
+file_path = '/tmp/inkwatchy_env'
+if os.path.exists(file_path):
+     os.remove(file_path)
+
+with open(file_path, 'w') as file:
+     file.write(pio_env)
 
 # Because cc1: warning: command-line option '-Wno-overloaded-virtual' is valid for C++/ObjC++ but not for C
 env.Append(CXXFLAGS=["-Wno-overloaded-virtual"])
