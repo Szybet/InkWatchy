60e9d926ab7bef093ace000115703d426b7a14a7
Szybet <53944559+Szybet@users.noreply.github.com>
2024-07-10 21:24:35 +0000

    improve compiling

--- resources/tools/other/compile/updateClean.sh
+++ resources/tools/other/compile/updateClean.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-rm -rf .pio/ dependencies.lock sdkconfig.* managed_components
-rm -rf resources/tools/other/in/*
-rm -rf resources/tools/other/out/*
-rm -rf resources/tools/fs/in/*
-rm -rf resources/tools/fs/out/*
-rm -rf resources/tools/fs/littlefs/*
+rm -rf .pio/build/ sdkconfig.* 
+# dependencies.lock
+# managed_components
+# rm -rf resources/tools/other/in/*
+# rm -rf resources/tools/other/out/*
+# rm -rf resources/tools/fs/in/*
+# rm -rf resources/tools/fs/out/*
+# rm -rf resources/tools/fs/littlefs/*
---
+++ resources/tools/other/compile/updateEasySetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source resources/tools/globalFunctions.sh
+
+pio_env=$(get_pio_env .vscode/launch.json)
+
+# We need it to compile the program that creates fonts
+pio pkg update -e $pio_env
+
+cd resources/tools/
+./generate.sh
+cd ../../
+
+pio run -e $pio_env
+pio run -e $pio_env
