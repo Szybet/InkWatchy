18286e604280e33bda27c4cd4dfc44fc40c88e26
Szybet <53944559+Szybet@users.noreply.github.com>
2024-07-10 21:25:05 +0000

    update compiling

--- resources/tools/other/compile/clean.sh
+++ resources/tools/other/compile/clean.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # I don't clean .pio/libdeps here
-rm -rf .pio/build/ dependencies.lock sdkconfig.* 
-# rm -rf managed_components
-# rm -rf resources/tools/other/in/*
-# rm -rf resources/tools/other/out/*
-# rm -rf resources/tools/fs/in/*
-# rm -rf resources/tools/fs/out/*
-# rm -rf resources/tools/fs/littlefs/*
+rm -rf .pio/ dependencies.lock sdkconfig.* 
+rm -rf managed_components
+rm -rf resources/tools/other/in/*
+rm -rf resources/tools/other/out/*
+rm -rf resources/tools/fs/in/*
+rm -rf resources/tools/fs/out/*
+rm -rf resources/tools/fs/littlefs/*
