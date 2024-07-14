e508e1dc298b2e2fa30cac27a9661269e702d251
Szybet <53944559+Szybet@users.noreply.github.com>
2024-07-14 13:20:39 +0000

    almost working demo

--- resources/tools/other/compile/demoMaker.sh
+++ resources/tools/other/compile/demoMaker.sh
@@ -1,79 +1,115 @@
 #!/bin/bash
 source resources/tools/globalFunctions.sh
 
-pio pkg install
-rm -rf resources/personal/books/* resources/personal/vault/*
-cp -r resources/demo/* resources/personal/
+#envList=("Watchy_2" "Watchy_3")
+envList=("Watchy_2")
 
-cd resources/tools/
-./generate.sh
-cd ../../
+for env in "${envList[@]}"; do
+    pio run --target clean -e $env
+    pio pkg install -e $env
+    pio pkg update -e $env
+done
 
-serial_port=$(extract_serial_port resources/tools/other/in/esptool)
-echo "Serial port detected: $serial_port"
+function generalThings {
+    cp -r resources/demo/* resources/personal/
 
-old_string="#define VAULT_PASSWORD \"\""
-new_string="#define VAULT_PASSWORD \"1\""
-filename="src/defines/confidential.h"
+    rm -rf src/defines/confidential.h
+    rm -rf src/defines/config.h
 
-sed -i "s/$old_string/$new_string/g" "$filename"
+    cd resources/tools/
+    ./generate.sh
+    cd ../../
 
-old_string="#define VAULT 0"
-new_string="#define VAULT 1"
-filename="src/defines/config.h"
+    cd resources/tools/fs
+    ./createFs.sh
+    cd ../../../
 
-sed -i "s/$old_string/$new_string/g" "$filename"
+    old_string="#define VAULT_PASSWORD \"\""
+    new_string="#define VAULT_PASSWORD \"1\""
+    filename="src/defines/confidential.h"
 
-old_string="#define BOOK 0"
-new_string="#define BOOK 1"
-filename="src/defines/config.h"
+    sed -i "s/$old_string/$new_string/g" "$filename"
 
-sed -i "s/$old_string/$new_string/g" "$filename"
+    old_string="#define VAULT 0"
+    new_string="#define VAULT 1"
+    filename="src/defines/config.h"
 
-old_string="#define WIFI_SSID1 \"\""
-new_string="#define WIFI_SSID1 \"hotspot\""
-filename="src/defines/confidential.h"
+    sed -i "s/$old_string/$new_string/g" "$filename"
 
-sed -i "s/$old_string/$new_string/g" "$filename"
+    old_string="#define BOOK 0"
+    new_string="#define BOOK 1"
+    filename="src/defines/config.h"
 
-old_string="#define WIFI_PASS1 \"\""
-new_string="#define WIFI_PASS1 \"12345678\""
-filename="src/defines/confidential.h"
+    sed -i "s/$old_string/$new_string/g" "$filename"
 
-sed -i "s/$old_string/$new_string/g" "$filename"
+    old_string="#define WIFI_SSID1 \"\""
+    new_string="#define WIFI_SSID1 \"hotspot\""
+    filename="src/defines/confidential.h"
 
-# To apply those things
-cd resources/tools/
-./generate.sh
-cd ../../
+    sed -i "s/$old_string/$new_string/g" "$filename"
 
-pio run
+    old_string="#define WIFI_PASS1 \"\""
+    new_string="#define WIFI_PASS1 \"12345678\""
+    filename="src/defines/confidential.h"
 
-#pio run
-#cd resources/tools/fs/
-#python3 generatePartTable.py
-#cd ../../../
+    sed -i "s/$old_string/$new_string/g" "$filename"
+}
 
-# This will succeed
-pio run
-resources/tools/other/in/esptool -p $serial_port erase_flash
-pio run -t upload --upload-port $serial_port
+generalThings
 
-sleep 10
+function compileEnv {
+    pio run -e $1
+    pio run -e $1
+}
 
-cd resources/tools/fs/
-./flashFs.sh
-cd ../../../
+for env in "${envList[@]}"; do
+    compileEnv $env
+done
 
-sleep 10
+function createEmptyBinary {
+    dd if=/dev/zero of=demo.bin bs=1M count=$1
+}
 
-cd resources/tools/other/
-in/esptool -p $serial_port read_flash 0x00000 0x400000 out/demo-program.bin
-#python ../fs/cleanPartition.py ../fs/in/partitions.csv out/demo-program.bin nvs
-#python ../fs/cleanPartition.py ../fs/in/partitions.csv out/demo-program.bin coredump
-sleep 5
-in/esptool -p $serial_port erase_flash
-sleep 5
-in/esptool -p $serial_port write_flash 0x00000 out/demo-program.bin
-cd ../../../
-zip -r -9 resources/tools/other/out/demo-source.zip . 1>/dev/null 2>/dev/null
+function hexToDec {
+  printf "%d\n" "$1"
+}
+
+function writeAtOffset {
+    # Input into output
+    local outputFile=$1
+    local inputFile=$2
+    local offset=$3
+    dd if="$inputFile" of="$outputFile" bs=1 seek="$offset" conv=notrunc
+}
+
+function assembleBinary {
+    binSize=$1
+    envName=$2
+    bootloaderAddress=$(hexToDec $3)
+
+    bootloaderPath=".pio/build/$envName/bootloader.bin"
+    partitionTablePath=".pio/build/$envName/partitions.bin"
+    firmwarePath=".pio/build/$envName/firmware.bin"
+    resourcesPath="resources/tools/fs/out/fs.bin"
+
+    createEmptyBinary $binSize
+
+    bootloaderAddress=$(hexToDec $3)
+    partitionTableAddress=$(hexToDec 0x19000)
+    firmwareAddress=$(hexToDec 0x20000)
+    resourceAddress=$(<resources/tools/fs/in/size.txt tr -d '\n')
+
+    writeAtOffset demo.bin $bootloaderPath $bootloaderAddress
+    writeAtOffset demo.bin $partitionTablePath $partitionTableAddress
+    writeAtOffset demo.bin $firmwarePath $firmwareAddress
+    writeAtOffset demo.bin $resourcesPath $resourceAddress
+
+    mv demo.bin resources/tools/other/out/demo/${envName}-demo.bin
+}
+
+rm -rf resources/tools/other/out/demo/
+mkdir -p resources/tools/other/out/demo/
+
+assembleBinary 4 Watchy_2 0x1000
+
+sync
