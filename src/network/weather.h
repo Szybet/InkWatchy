e6e810565d3b90bc1e1c8475cc20f68f99938698
Szybet <53944559+Szybet@users.noreply.github.com>
2024-07-15 11:21:18 +0000

    initial not working weather

--- src/network/weather.h
+++ src/network/weather.h
@@ -3,28 +3,11 @@
 
 #include "defines/defines.h"
 
-struct savedWeatherData {
-    uint32_t dt; // time, unix, use strTime
-    float temp; // C, depends
-    float minTemp; // C, depends
-    float pressure; // hPa
-    uint8_t humidity; // %
-    uint16_t weatherConditionId; // Use weatherConditionIdToStr
-    uint8_t cloudsPerc; // % of clouds
-    float windSpeed; // m/s
-    float windGusts; // m/s
-    uint32_t visibility;
-    uint8_t pop; // Probability of precipitation in %
-    uint32_t sunrise; // time, unix
-    uint32_t sunset; // time, unix
-};
+#if WEATHER_INFO
 
-#define WEATHER_PER_DAY 8
 extern bool isWeatherAvailable;
-extern savedWeatherData weatherDataDays[MAX_DAYS][WEATHER_PER_DAY]; // Days / Data for these days
 
 void syncWeather();
-void saveWeatherData();
 void loadWeatherData();
 
 #if DEBUG
