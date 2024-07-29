764a6d2191c261dadab58305c194cd2cc980d68b
Szybet <53944559+Szybet@users.noreply.github.com>
2024-07-29 19:39:19 +0000

    fix compiling for temp for the v3

--- src/hardware/temp/temp.cpp
+++ src/hardware/temp/temp.cpp
@@ -1,7 +1,7 @@
 #include "temp.h"
 
 #if TEMP_CHECKS_ENABLED
-
+#if ATCHY_VER == WATCHY_2
 #ifdef __cplusplus
 extern "C"
 {
@@ -16,6 +16,23 @@ float getTemp()
 {
     return float(temprature_sens_read() - 32) / 1.8 + float(TEMP_REAL_OFFSET);
 }
+#else
+#include "driver/temperature_sensor.h"
+
+float getTemp()
+{
+    // https://docs.espressif.com/projects/esp-idf/en/stable/esp32s3/api-reference/peripherals/temp_sensor.html
+    // Performance of this is not great
+    temperature_sensor_handle_t temp_handle = NULL;
+    temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(0, 80);
+    ESP_ERROR_CHECK(temperature_sensor_install(&temp_sensor_config, &temp_handle));
+    ESP_ERROR_CHECK(temperature_sensor_enable(temp_handle));
+    float tsens_out;
+    ESP_ERROR_CHECK(temperature_sensor_get_celsius(temp_handle, &tsens_out));
+    ESP_ERROR_CHECK(temperature_sensor_disable(temp_handle));
+    return tsens_out;
+}
+#endif
 
 RTC_DATA_ATTR uint8_t fixCounts = 0;
 void screenTempFix()
