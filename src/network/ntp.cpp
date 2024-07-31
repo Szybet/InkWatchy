c37cee6bc63dd429cbdf8eba7d9bc3c096b97b90
Szybet <53944559+Szybet@users.noreply.github.com>
2024-07-31 20:24:49 +0000

    improve ntp

--- src/network/ntp.cpp
+++ src/network/ntp.cpp
@@ -6,23 +6,22 @@ int ntpTries = 0;
 
 void syncNtp(bool doDriftThings)
 {
+    unsetenv("TZ");
+    setenv("TZ", "", 1);
+    tzset();
     debugLog("Running syncNtp");
     WiFiUDP ntpUDP;
     NTPClient timeClient(ntpUDP);
     timeClient.begin();
     if (timeClient.forceUpdate() == true)
     {
-        //timeClient.setTimeOffset(TIME_OFFSET_S);
+        // timeClient.setTimeOffset(TIME_OFFSET_S);
         time_t epochTime = timeClient.getEpochTime();
-        SRTC.read(timeRTC);
-        time_t currentTime = SRTC.doMakeTime(timeRTC); // Save the time to a var and update it afterwards for better accuracy
         if (firstNTPSync == true)
         {
             debugLog("firstNTPSync is true, running again");
             firstNTPSync = false;
-            initialRTCTime = currentTime;
-            SRTC.doBreakTime(epochTime, timeRTC);
-            saveRTC(timeRTC);
+            initialRTCTime = epochTime;
             timeClient.end();
             syncNtp();
             return;
@@ -30,13 +29,12 @@ void syncNtp(bool doDriftThings)
         else
         {
             // It runs it 2 times then compares the time
-            int difference = abs(epochTime - currentTime);
+            int difference = abs(epochTime - initialRTCTime);
             debugLog("difference: " + String(difference));
-            if (difference > 5)
+            if (difference > 30)
             {
                 debugLog("Difference too high, running ntp once more");
-                SRTC.doBreakTime(epochTime, timeRTC);
-                saveRTC(timeRTC);
+                initialRTCTime = epochTime;
                 timeClient.end();
                 syncNtp();
                 return;
@@ -44,7 +42,6 @@ void syncNtp(bool doDriftThings)
         }
         debugLog("NTP success");
         debugLog("NTP time: " + timeClient.getFormattedTime());
-        currentTime = initialRTCTime;
 
         /* GuruSR:
         SmallRTC doesn't cause time to drift.  The drift values are all reset on init, they never change anything when like that.  Are you using the SRTC's make and break time functions, if not, that is what is happening, the standard makeTime and breakTime are not compliant to time.h, they add month and day onto the values.  With the correct TimeZone (TZ) used, using localTime against the RTC having UTC values, you'll get accurate DST calculations.
@@ -52,11 +49,14 @@ void syncNtp(bool doDriftThings)
         I haven't looked at your code, but the make and break Time functions outside of SmallRTC don't follow time.h for values, so the day and month will increase.
         */
 
-        SRTC.doBreakTime(epochTime, timeRTC);
-        saveRTC(timeRTC);
-        #if TIME_DRIFT_CORRECTION
-        if(doDriftThings == true) {
-            if(SRTC.checkingDrift() == true) {
+        tmElements_t timeTmp;
+        SRTC.doBreakTime(epochTime, timeTmp);
+        saveRTC(timeTmp);
+#if TIME_DRIFT_CORRECTION
+        if (doDriftThings == true)
+        {
+            if (SRTC.checkingDrift() == true)
+            {
                 // Drift is going on
                 SRTC.endDrift(timeRTC);
                 uint32_t driftValue = SRTC.getDrift();
@@ -64,15 +64,18 @@ void syncNtp(bool doDriftThings)
                 debugLog("isFast: " + String(driftIsFast) + " drift value: " + String(driftValue));
                 fsSetString(CONF_DRIFT, String(driftValue));
                 fsSetString(CONF_DRIFT_FAST, String(driftIsFast));
-            } else {
+            }
+            else
+            {
                 // Drift is not going on
                 SRTC.beginDrift(timeRTC);
             }
         }
-        #endif
+#endif
 
         timeClient.end();
-        readRTC(); // After syncing time, remake the alarm
+        debugLog("Reading rtc from ntp");
+        readRTC(); // After syncing time, remake the timezone
     }
     else
     {
