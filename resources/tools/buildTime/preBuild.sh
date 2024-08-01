c1a0391ca5ac99d34aa4f379232dd81e390e2893
Szybet <53944559+Szybet@users.noreply.github.com>
2024-08-01 09:13:43 +0000

    improve compiling performance

--- resources/tools/buildTime/preBuild.sh
+++ resources/tools/buildTime/preBuild.sh
@@ -5,14 +5,32 @@ source resources/tools/globalFunctions.sh
 pio_env=$(get_pio_env .vscode/launch.json)
 
 cd .pio/build/$pio_env
-ninja -k 0 || true
-#ninja || true
-#ninja || true
 
+#ninja -k 0 || true
+#exit 0
 
-FILE="~/.platformio/penv/.espidf-5.1.2/lib/python3.12/site-packages/pyserial-3.5.dist-info"
+check_and_run() {
+    TARGET="$1"
+    FILE="$2"
 
-if [ ! -e "$FILE" ]; then
-    echo "Pyserial doesn't exist, trying to install it..."
-    /root/.platformio/penv/.espidf-5.1.2/bin/python -m pip install pyserial
-fi
+    if [ ! -f "$FILE" ]; then
+        echo "Missing $FILE" >&2
+        ninja "$TARGET" -k 0 || true
+    fi
+}
+
+check_and_run "https_server.crt.S" "https_server.crt.S"
+
+check_and_run "rmaker_mqtt_server.crt.S" "rmaker_mqtt_server.crt.S"
+
+check_and_run "rmaker_claim_service_server.crt.S" "rmaker_claim_service_server.crt.S"
+
+check_and_run "rmaker_ota_server.crt.S" "rmaker_ota_server.crt.S"
+
+#FILE="~/.platformio/penv/.espidf-5.1.2/lib/python3.12/site-packages/pyserial-3.5.dist-info"
+
+#if [ ! -e "$FILE" ]; then
+#    echo "Pyserial doesn't exist, trying to install it..."
+#    /root/.platformio/penv/.espidf-5.1.2/bin/python -m pip install pyserial
+#fi
+
