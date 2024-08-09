437c969c5f46aaf94cb2e5e63e0cec02e5cdcdd5
Szybet <53944559+Szybet@users.noreply.github.com>
2024-08-09 19:34:16 +0000

    cleaning of sdkconfig

--- resources/tools/buildTime/preBuild.sh
+++ resources/tools/buildTime/preBuild.sh
@@ -9,23 +9,24 @@ cd .pio/build/$pio_env
 #ninja -k 0 || true
 #exit 0
 
-check_and_run() {
-    TARGET="$1"
-    FILE="$2"
+# check_and_run() {
+#     TARGET="$1"
+#     FILE="$2"
 
-    if [ ! -f "$FILE" ]; then
-        echo "Missing $FILE" >&2
-        ninja "$TARGET" -k 0 || true
-    fi
-}
+#     if [ ! -f "$FILE" ]; then
+#         echo "Missing $FILE" >&2
+#         ninja "$TARGET" -k 0 || true
+#     fi
+# }
 
-check_and_run "https_server.crt.S" "https_server.crt.S"
-check_and_run "rmaker_mqtt_server.crt.S" "rmaker_mqtt_server.crt.S"
-check_and_run "rmaker_claim_service_server.crt.S" "rmaker_claim_service_server.crt.S"
-check_and_run "rmaker_ota_server.crt.S" "rmaker_ota_server.crt.S"
+#check_and_run "https_server.crt.S" "https_server.crt.S"
+#check_and_run "rmaker_mqtt_server.crt.S" "rmaker_mqtt_server.crt.S"
+#check_and_run "rmaker_claim_service_server.crt.S" "rmaker_claim_service_server.crt.S"
+#check_and_run "rmaker_ota_server.crt.S" "rmaker_ota_server.crt.S"
 
 #cp managed_components/espressif__esp_insights/server_certs/https_server.crt .pio/build/$pio_env/https_server.crt
 #cp managed_components/espressif__esp_rainmaker/server_certs/rmaker_mqtt_server.crt .pio/build/$pio_env/
 #cp managed_components/espressif__esp_rainmaker/server_certs/rmaker_claim_service_server.crt .pio/build/$pio_env/
 #cp managed_components/espressif__esp_rainmaker/server_certs/rmaker_ota_server.crt .pio/build/$pio_env/
 
+cp resources/tools/buildTime/sdkconfig_${pio_env}.defaults sdkconfig.defaults
