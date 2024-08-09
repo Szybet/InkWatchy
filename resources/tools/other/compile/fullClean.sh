1384d307ae0d4f8d96b166ce2c826841e9562bcd
Szybet <53944559+Szybet@users.noreply.github.com>
2024-08-09 20:59:06 +0000

    progress with building

--- resources/tools/other/compile/fullClean.sh
+++ resources/tools/other/compile/fullClean.sh
@@ -4,7 +4,6 @@ rm -rf .pio/ dependencies.lock sdkconfig.*
 rm -rf .platformio/.cache .platformio/platforms .platformio/packages/framework-e*
 rm -rf .esp-idf/esp-idf/*
 rm -rf .esp-idf/espressif/*
-touch .esp-idf/esp-idf/.gitkeep
 touch .esp-idf/espressif/.gitkeep
 rm -rf managed_components components
 rm -rf resources/tools/other/in/*
@@ -12,3 +11,5 @@ rm -rf resources/tools/other/out/*
 rm -rf resources/tools/fs/in/*
 rm -rf resources/tools/fs/out/*
 rm -rf resources/tools/fs/littlefs/*
+echo "Waiting for 20 seconds for platformio to shut up"
+sleep 20
