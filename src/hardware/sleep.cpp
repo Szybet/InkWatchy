59d58b189603346239f4fe86adbb9a702e2a7876
Szybet <53944559+Szybet@users.noreply.github.com>
2024-09-20 22:37:26 +0000

    add comment

--- src/hardware/sleep.cpp
+++ src/hardware/sleep.cpp
@@ -100,6 +100,7 @@ void goSleep()
     runLpCore();
     // This enables the subsystem, so it doesn't shut it down or something
     // https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/sleep_modes.html#ulp-coprocessor-wakeup
+    // TODO: maybe this https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/sleep_modes.html#power-down-of-rtc-peripherals-and-memories
     ESP_ERROR_CHECK(esp_sleep_enable_ulp_wakeup());
 #else
     display.hibernate();
