7d5a77dbe7438e84b50b0c5601f709aa0c7afa3d
Szybet <53944559+Szybet@users.noreply.github.com>
2024-10-06 18:49:25 +0000

    description

--- src/defines/templates/laitnedifnoc-template.h
+++ src/defines/templates/laitnedifnoc-template.h
@@ -23,6 +23,7 @@ STATIC_WIFI_CRED wifi_credential10 = {.ssid = "hotspot", .password = "12345678"}
 #define VAULT_PASSWORD "" // This must be a numerical PIN, don't start it with 0
 
 // If you want just a path, implement it yourself, look up the script and the program arguments
+// Link to your ICS file to download it
 #define CALENDAR_URL "" // https://MyIcsFileSomewhere.toDownload
 
 #endif
