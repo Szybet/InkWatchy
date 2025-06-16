4aeeef8c48bdc33da20e65031e475f636b5238b9
themkoi <Pascalsch3@gmail.com>
2025-06-16 16:57:45 +0200

    slint

--- components/rusty/crates/general_page/src/lib.rs
+++ components/rusty/crates/general_page/src/lib.rs
@@ -1,6 +1,6 @@
 // #![no_std]
 
-use slint::SharedString;
+use slint::{ModelRc, SharedString, VecModel};
 
 slint::include_modules!();
 
@@ -38,16 +38,18 @@ pub fn get_general_page() -> GeneralPage {
         let an_an_ui = ui_clone.upgrade().unwrap();
         let an_ui = an_an_ui.global::<Adapter>();
         if !an_ui.get_buttons_shown() {
-            let mut scroll = an_ui.get_scroll();
             //let vh = an_ui.get_scroll_viewport_height();
 
             //println!("vh: {:?}", vh);
 
             if btn == Button::Up {
-                scroll += 10.0;
+                an_an_ui.invoke_handleUpButton();
             }
             if btn == Button::Down {
-                scroll -= 10.0;
+                an_an_ui.invoke_handleDownButton();
+            }
+            if btn == Button::Menu {
+                an_an_ui.invoke_handleMenuButton();
             }
             /*
             if scroll < 0.0 {
@@ -57,9 +59,11 @@ pub fn get_general_page() -> GeneralPage {
                 scroll = vh;
             }
              */
-
-            an_ui.set_scroll(scroll);
         }
+    });
+
+    ui.on_button_pressed(move |button: SharedString| {
+        println!("button pressed: {}", button);
     });
 
     ui
@@ -72,9 +76,73 @@ mod tests {
     #[test]
     fn run_std() {
         let ui = get_general_page();
-        ui.global::<Adapter>().set_title_text("Test title is here".into());
+        ui.global::<Adapter>()
+            .set_title_text("Test title is here".into());
         // ui.global::<Adapter>().set_title_enabled(false);
         ui.global::<Adapter>().set_main_text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".into());
+        let items = vec![
+            ButtonItem {
+                button_name: SharedString::from("Loremdsauiohdsauiod"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Ipsum"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Dolor"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Sit"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Amet"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Consectetur"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Adipiscing"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Elit"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Sed"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Do"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Eiusmod"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Tempor"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Incididunt"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Ut"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Labore"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Et"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Dolore"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Magna"),
+            },
+            ButtonItem {
+                button_name: SharedString::from("Aliqua"),
+            },
+        ];
+
+        let model = ModelRc::new(VecModel::from(items));
+        ui.set_button_items(model);
+
         ui.run().unwrap();
     }
 }
