a1b2402ac6d29fa2443d600ef835417b981db09b
Szybet <53944559+Szybet@users.noreply.github.com>
2025-07-21 21:40:36 +0000

    lp core check

--- resources/tools/buildTime/preBuild.py
+++ resources/tools/buildTime/preBuild.py
@@ -12,6 +12,18 @@ if str(pio_env) == "Unknown":
      print("Select the proper PlatformIO environment; it is currently unknown!", file=sys.stderr)
      sys.exit(1)
 
+# Lp core check
+if str(pio_env) == "Yatchy":
+     path = "resources/tools/fs/littlefs/other/yatchy-lp-program.bin"
+     if not os.path.isfile(path):
+          with open("src/defines/condition.h") as f:
+               for line in f:
+                    if "#define LP_CORE" in line:
+                         if "1" in line:
+                              print("LP_CORE is defined as 1 but the binary is missing")
+                              sys.exit(1)
+                         break     
+
 file_path = '/tmp/inkwatchy_env'
 if os.path.exists(file_path):
      os.remove(file_path)
