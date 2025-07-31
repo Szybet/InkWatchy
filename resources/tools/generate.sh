2dab4fca715bf880a98f8317a94318513d9a9d2c
Szybet <53944559+Szybet@users.noreply.github.com>
2025-07-31 06:00:18 +0000

    fix generate a bit

--- resources/tools/generate.sh
+++ resources/tools/generate.sh
@@ -17,10 +17,15 @@ cd images/
 cd ../
 
 # Need to make sure we have fontconvert installed
-cd ../../
 source globalFunctions.sh
-pio_env=$(get_pio_env ../../.vscode/launch.json)
-pio pkg install -e $pio_env
+cd ../../
+pio_env=$(get_pio_env .vscode/launch.json)
+if [ ! -d ".pio/libdeps/$pio_env/Adafruit\ GFX\ Library/fontconvert/" ]; then
+    echo "Fontconvert does not exist, installing libs"
+  pio pkg install -e $pio_env
+else
+  echo "Fontconvert exist, not installing libs"
+fi
 cd resources/tools
 
 echo -e ''
