ebc3e6e17f3500bf30c5f177ab4122bc8f476c08
izzylaif <easylifeeasylife@gmail.com>
2025-09-10 15:35:18 +0100

    Update setTimeWatch.cpp

--- src/ui/setTimeWatch/setTimeWatch.cpp
+++ src/ui/setTimeWatch/setTimeWatch.cpp
@@ -1,13 +1,33 @@
 #include "setTimeWatch.h"
 #include "rtcMem.h"
 #include "../../reUse/setTime/setTime.h"
+#include "rtc.h" // for timeRTCUTC0, timeZoneOffset
 
 static bool setTimeWatch_initialized = false;
 
+// Helper to add seconds to hour/minute (no day wrap, for simplicity)
+void addSecondsToHourMinute(uint8_t baseHour, uint8_t baseMinute, int32_t seconds, int& outHour, int& outMinute) {
+    int totalMinutes = baseHour * 60 + baseMinute + seconds / 60;
+    // Wrap around 24 hours
+    if (totalMinutes < 0) totalMinutes += 24 * 60;
+    if (totalMinutes >= 24 * 60) totalMinutes -= 24 * 60;
+    outHour = (totalMinutes / 60) % 24;
+    outMinute = totalMinutes % 60;
+}
+
 void initSetTimeWatch() {
     if (!setTimeWatch_initialized) {
-        setTimeHour = timeRTCUTC0.Hour;
-        setTimeMinute = timeRTCUTC0.Minute;
+        // Correct: subtract timezone offset (if offset is negative for GMT+1)
+        int localHour, localMinute;
+        addSecondsToHourMinute(
+            timeRTCUTC0.Hour,
+            timeRTCUTC0.Minute,
+            -(int32_t)(timeZoneOffset), // <-- FIXED SIGN
+            localHour,
+            localMinute
+        );
+        setTimeHour = localHour;
+        setTimeMinute = localMinute;
         initSetTime();
         setTimeWatch_initialized = true;
     }
@@ -19,11 +39,21 @@ void loopSetTimeWatch() {
 
 void exitSetTimeWatch() {
     setTimeWatch_initialized = false;
-    exitSetTime(); // <-- Commit UI values to globals!
+    exitSetTime(); // Commit UI edits
+
+    // Correct: add timezone offset to get back to UTC
+    int utcHour, utcMinute;
+    addSecondsToHourMinute(
+        setTimeHour,
+        setTimeMinute,
+        (int32_t)(timeZoneOffset), // <-- FIXED SIGN
+        utcHour,
+        utcMinute
+    );
+
     tmElements_t newTime = timeRTCUTC0;
-    // Use the values selected by the user:
-    newTime.Hour = setTimeHour;
-    newTime.Minute = setTimeMinute;
+    newTime.Hour = utcHour;
+    newTime.Minute = utcMinute;
     newTime.Second = 0;
     saveRTC(newTime);
     readRTC();
