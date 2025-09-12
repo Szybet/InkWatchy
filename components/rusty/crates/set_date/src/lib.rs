b44f0be7b7a6fb1b862853fecd6df19af6bd00d5
Szybet <53944559+Szybet@users.noreply.github.com>
2025-09-12 20:49:30 +0200

    finish?

--- components/rusty/crates/set_date/src/lib.rs
+++ components/rusty/crates/set_date/src/lib.rs
@@ -7,20 +7,26 @@ slint::include_modules!();
 
 pub fn get_date_picker() -> DatePicker {
     let ui = DatePicker::new().unwrap();
-
-    // Create a sample date for demonstration
-    let current_date = Date {
-        year: 2025,
-        month: 9, // September
-        day: 12,
-    };
-
-    // Set the initial date for the DatePicker
-    ui.set_date(current_date.clone()); // Clone for the first use
-    // Set today's date for highlighting
-    ui.set_today_date(current_date);
-
     ui
+}
+
+pub fn date_picker_set_date(ui: &DatePicker, year: u16, month: u8, day: u8) {
+    let new_date = Date {
+        year: year as i32,
+        month: month as i32,
+        day: day as i32,
+    };
+    ui.set_date(new_date);
+}
+
+// year, month, day
+pub fn date_picket_get_date(ui: &DatePicker) -> (u16, u8, u8) {
+    let current_date = ui.get_date();
+    (
+        current_date.year as u16,
+        current_date.month as u8,
+        current_date.day as u8,
+    )
 }
 
 #[cfg(test)]
@@ -30,6 +36,8 @@ mod tests {
     #[test]
     fn run_std() {
         let ui = get_date_picker();
+        date_picker_set_date(&ui, 2010, 10, 10);
+        println!("Date set: {:?}", date_picket_get_date(&ui));
         ui.run().unwrap();
     }
 }
