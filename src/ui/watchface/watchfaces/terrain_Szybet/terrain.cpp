df489fecc40def89dc248b16f9e1cdd9d9744945
Szybet <53944559+Szybet@users.noreply.github.com>
2025-12-26 22:02:13 +0000

    terrain time

--- src/ui/watchface/watchfaces/terrain_Szybet/terrain.cpp
+++ src/ui/watchface/watchfaces/terrain_Szybet/terrain.cpp
@@ -3,8 +3,27 @@
 
 #if WATCHFACE_TERRAIN
 
+#define TIME_CORD_X 7
+#define TIME_CORD_Y 118
+#define TIME_CORD TIME_CORD_X, TIME_CORD_Y
+#define TIME_WIDTH 145
+#define TIME_HEIGHT 30
+
+// #define TIME_FONT getFont("terrain/font_terrain20")
+#define TIME_FONT getFont("terrain/octosquares21")
+
 void showTimeFull()
 {
+    setTextSize(1);
+    setFont(TIME_FONT);
+    // First clear because font sizes
+    dis->fillRect(TIME_CORD_X, TIME_CORD_Y - TIME_HEIGHT, TIME_WIDTH, TIME_HEIGHT, SCWhite);
+    writeTextReplaceBack(getHourMinute(timeRTCLocal), TIME_CORD);
+}
+
+static void initWatchface()
+{
+    writeImageN(0, 0, getImg("terrain/watchface"));
 }
 
 const watchfaceDefOne terrainDefOne = {
@@ -13,7 +32,7 @@ const watchfaceDefOne terrainDefOne = {
     .drawDay = []() {},
     .drawMonth = []() {},
     .showTimeFull = showTimeFull,
-    .initWatchface = []() {},
+    .initWatchface = initWatchface,
     .drawBattery = []() {},
     .manageInput = [](buttonState bt)
     {
